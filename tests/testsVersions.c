bfa185846e977ee3310b65af385e67d3c039cce1
ZarzyckiAlexis <101389918+ZarzyckiAlexis@users.noreply.github.com>
2022-08-06 15:13:37 +0200

    Update
    Mise à jour : global.c, global.h => Deux nouvelles fonction ; suppressions de toutes les tables, et une fonction permettant de voir si une données est présente dans la table.
    
    Mise à jour versions.c, versions.h, testVersions.c => Refonte de certains code du programme qui perdait l'allocation mémoire, finition de la fonction ajout de version en DB (à fix bug ID auto-incrémenter), ajout de quelques tests de développements.

--- tests/testsVersions.c
+++ tests/testsVersions.c
@@ -1,4 +1,6 @@
 #include "..\includes\versions.h"
+#include "..\includes\moteurs.h"
+#include "..\includes\versionsMoteurs.h"
 #include "..\unity\unity.h"
 
 void setup(){
@@ -6,6 +8,8 @@ void setup(){
 
 // test de l'existence de la table versions
 void testExistTableVersions(){
+    destroyAllTable();
+    createTableVersions();
     int resultat;
     resultat = existTableVersions(); // Existe -> retourne 1
     TEST_ASSERT_EQUAL(1, resultat); // Elle existe
@@ -13,7 +17,7 @@ void testExistTableVersions(){
 
 void testNotExistTableVersions(){
     int resultat;
-    executerCommandeSQL("DROP TABLE versions"); // On supprime la table
+    destroyAllTable();
     resultat = existTableVersions(); // Existe pas -> retourne 0
     TEST_ASSERT_EQUAL(0, resultat); // Elle n'existe pas
 }
@@ -63,16 +67,36 @@ void testTableVersions(){
     free(nbElements);
 }
 
+void testAjoutVersionErrorNameVersion(){
+    destroyAllTable();
+    createTableVersions();
+    executerCommandeSQL("CREATE TABLE moteurs(idMoteur INT, cylindree INT, nombreCylindres INT, puissance INT, typeCarburant VARCHAR(20), PRIMARY KEY(idMoteur) );");
+    executerCommandeSQL("CREATE TABLE versions_moteurs(idMoteur INT, idVersion INT, PRIMARY KEY(idMoteur, idVersion), FOREIGN KEY(idMoteur) REFERENCES moteurs(idMoteur), FOREIGN KEY(idVersion) REFERENCES versions(idVersion));");
+    executerCommandeSQL("INSERT INTO `moteurs` (`idMoteur`, `cylindree`, `nombreCylindres`, `puissance`, `typeCarburant`) VALUES ('1', '1', '1', '1', 'essence');");
+    executerCommandeSQL("INSERT INTO `versions_moteurs` (`idMoteur`, `idVersion`) VALUES ('1', '1');");
+    char name[100] = "celica";
+    char *errors = (char *)malloc(1000); // on réserve un emplacement mémoire pour la gestion des erreurs
+    char *messages = (char *)malloc(1000);
+    strcpy(errors, "\n"); // On initialise le contenu de la liste d'erreurs
+    addVersions("wouaise", name, "1", errors);
+    strcpy(messages, "Cette marques n'existe pas!");
+    printf("\n 1: %s \n 2: %s \n", messages, errors);
+    // Errors ne renvoit rien ? Versions.c il renvoit bien ?
+    TEST_ASSERT_EQUAL(0, strcmp(messages, errors));
+    free(errors);
+    free(messages);
+}
 
 
 int main(void){
     UNITY_BEGIN();
+    RUN_TEST(testExistTableVersions);
     RUN_TEST(testNotExistTableVersions);
     RUN_TEST(testCreateTableVersions);
-    RUN_TEST(testExistTableVersions);
-    RUN_TEST(testTableNotEmpty);
     RUN_TEST(testTableEmpty);
+    RUN_TEST(testTableNotEmpty);
     RUN_TEST(testTableVersions);
+    RUN_TEST(testAjoutVersionErrorNameVersion);
     UNITY_END();
     return 0;
 }
