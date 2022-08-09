ca79764f36f46545fc5d847acccf2fb184264bcb
NoahVerly <101389611+NoahVerly@users.noreply.github.com>
2022-08-09 12:18:32 +0200

    ajout de la fonction ajouter des utilisateur
    j'ai fait la fonction ajouter pour ajouter les utilisateur avec les fonction qui vont avec et j'ai rajouter une fonction lister les utilisateur présent pour aider quand on devra supprimer des utilisateur ou modifier

---
+++ includes/users.h
@@ -0,0 +1,30 @@
+/*
+Auteur : Noah Verly
+But :   Fichier contenant tout les #includes,#define
+        et déclaration des fonctions
+*/
+
+// INCLUDE
+#include <locale.h>  //Ceci permet de changer l'encodage
+#include <stdio.h>   //Ouput,input/ tout les fonction par rapport aux fichiers
+#include <stdlib.h>  //exit
+#include <conio.h>   //clrscr
+#include <string.h>  // Pour les chaines de carat�eres
+#include <windows.h> // Sleep
+
+// DEFINE
+#define DIM 60
+#define NAME_FICHIER "users.txt"
+#define LIBRE_VIDE 0 
+#define SIMPLE 1
+#define ADMIN 2
+
+/*FOCNTION*/
+// fonction pour lister les utilisateur
+void lister_uti();
+// fonction pour l'ajout
+void menu_ajouter(char *rang);
+unsigned ajouter(FILE *fichier,char *log, char *mdp, int dim, char *rang);
+// fonction commune
+unsigned check_existe(char *log, int dim, FILE *fichier);
+void chiffrement(char *log, char *mdp, int dim);
