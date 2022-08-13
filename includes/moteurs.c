607aa9a4f1a385042538c931ad5a7ae88bcddaed
MatteoCastin <la218187@student.helha.be>
2022-08-13 16:38:20 +0200

    Suppression de la fonction creationListePourMoteurs
    - Suppression de la fonction
    - Modification des tests l'utilisant

--- includes/moteurs.c
+++ includes/moteurs.c
@@ -116,26 +116,43 @@ void recuperationDesInfosVersions(char tableauInfosVersions[][51], int tableauId
     }
 }
 
-// Fonction pour récupérer les versions d'un moteur demander
-void creationListePourMoteurs(int idMoteur, int *nbVersions)
+int main(void)
 {
+    int idMoteur = 0, nbVersions = 0;
+    printf("Entre l'id du moteur : ");
+    scanf("%d", &idMoteur);
+    
     MYSQL_RES *res = NULL;
 
     // Récupération des ids des différentes versions en DB
     // Si après le passage nbVersions = -1 alors le moteur n'existe pas
     // Si après le passage nbVersions = 0 alors le moteur n'a pas de modèle
-    res = recuperationDesVersions(idMoteur, nbVersions);
+    res = recuperationDesVersions(idMoteur, &nbVersions);
 
     // Si il y a 0 version cela veut dire que se moteur ne contient pas de version
-    if (*nbVersions > 1)
+    if (nbVersions > 1)
 
     { // Tableau contenant tout les ids des versions contenant le moteur choisi
-        int tableauIdVersions[*nbVersions];
+        int tableauIdVersions[nbVersions];
         // Tableau qui contiendra les infos récupérer pour chaque version
-        char tableauInfosVersions[*nbVersions * 4][51];
+        char tableauInfosVersions[nbVersions * 4][51];
         // Utilisation des résultats pour récupérer les ids dans un tableau
         recuperationDuResultat(tableauIdVersions, res);
         // Récupération des infos de chaque version contenant le moteur choisi
-        recuperationDesInfosVersions(tableauInfosVersions, tableauIdVersions, *nbVersions);
+        recuperationDesInfosVersions(tableauInfosVersions, tableauIdVersions, nbVersions);
+    }
+
+    switch (nbVersions)
+    {
+    case 0:
+        printf("Aucune version pour ce moteur");
+        break;
+    case -1:
+        printf("Aucun moteur ne possède cette identifiant");
+        break;
+    default:
+        printf("Des versions ont été trouvés");
+        printf("%d",nbVersions);
+        break;
     }
 }
